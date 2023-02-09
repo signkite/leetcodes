7dacb7bc48ae58658dec9bb6653767f0adb666d0
signkite <50399725+signkite@users.noreply.github.com>
2023-02-10 08:33:47 +0900

    Time: 99 ms (92.43%), Space: 18.5 MB (89.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,11 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = collections.Counter(nums)
         output = []
-        for num, _ in counter.most_common():
-            if k == 0:
-                break
-            
-            k -= 1
+        for num, _ in counter.most_common(k):
             output.append(num)
             
         return output
