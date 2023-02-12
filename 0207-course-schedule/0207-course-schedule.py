dd9b9c3a44155af8bf25e256e54575fca8bcb399
signkite <50399725+signkite@users.noreply.github.com>
2023-02-12 21:27:12 +0900

    Time: 205 ms (16.98%), Space: 16.7 MB (59.96%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        for end, start in prerequisites:
+            graph[start].append(end)
+    
+        checked = []  # 사이클이 없다고 확인이 완료된 노드는 재방문할 필요 없음
+        visited = []  # 방문 경로 체크
+        
+        def dfs(dpt):
+            if dpt in checked:
+                return True
+            elif dpt in visited:
+                return False
+            
+            visited.append(dpt)
+            for dst in graph[dpt]:
+                if dst in checked:
+                    continue
+                elif dpt in checked:
+                    return False
+                
+                if not dfs(dst):
+                    return False
+            
+            checked.append(dpt)
+            visited.remove(dpt)
+            return True
+        
+        for start in list(graph.keys()):
+            if not dfs(start):
+                return False
+        return True
+        
